c817045a3cd5f1b94d5b46cce48647658c04da5e
Golang Uzbekistan <golanguzb70@gmail.com>
2023-06-24 11:32:58 +0500

    update scrips/crud.sh

--- scripts/crud.sh
+++ scripts/crud.sh
@@ -3,6 +3,5 @@
 source ./config/maker.env
 echo $TEMPLATE_PATH
 echo $GO_MOD_URL
-sudo chmod 770 $TEMPLATE_PATH/github.com/golanguzb70/go-gin-bearer-auth-postgres-monolithic-template-crud.sh
-$TEMPLATE_PATH/github.com/golanguzb70/go-gin-bearer-auth-postgres-monolithic-template-crud.sh
+bash $TEMPLATE_PATH/go-gin-bearerauth-postgres-monolithic-template-crud.sh
 make swag_init
