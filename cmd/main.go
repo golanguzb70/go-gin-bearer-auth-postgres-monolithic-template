ecb62342094ac425e395286bf7bed977015500c7
Golang Uzbekistan <golanguzb70@gmail.com>
2023-12-12 15:10:50 +0500

    upate cmd logic

--- cmd/main.go
+++ cmd/main.go
@@ -1,8 +1,29 @@
 package main
 
-import "github.com/golanguzb70/go-gin-bearer-auth-postgres-monolithic-template/app"
+import (
+	"log"
+
+	"github.com/golanguzb70/go-gin-bearer-auth-postgres-monolithic-template/api"
+	"github.com/golanguzb70/go-gin-bearer-auth-postgres-monolithic-template/config"
+	"github.com/golanguzb70/go-gin-bearer-auth-postgres-monolithic-template/pkg/db"
+	"github.com/golanguzb70/go-gin-bearer-auth-postgres-monolithic-template/pkg/logger"
+	"github.com/golanguzb70/go-gin-bearer-auth-postgres-monolithic-template/storage"
+)
 
 func main() {
-	app := app.New()
-	app.Run()
+	cfg := config.Load()
+	logger := logger.New(cfg.LogLevel)
+
+	db, err := db.New(cfg)
+	if err != nil {
+		logger.Error("Error while connecting to database", err)
+	} else {
+		logger.Info("Successfully connected to database")
+	}
+
+	router := api.New(logger, cfg, storage.New(db, logger, cfg))
+
+	if err := router.Run(":" + cfg.HTTPPort); err != nil {
+		log.Fatal("failed to run http server", err)
+	}
 }
